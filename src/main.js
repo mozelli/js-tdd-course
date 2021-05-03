f02526b32612a8971fae51f4fbc1e3f25755801a
Joao Mozelli Neto <joaomozelli@hotmail.com>
2021-05-03 19:43:21 -0300

    Add src folder and create main file

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const a = 3;
+const b = 4;
+
+const nome = 'Olá Mndo';
+
+function sum() {
+  const result = a + b;
+  return result;
+}
+
+function myname() {
+  return nome;
+}
+
+myname();
+sum();
